d6c6e8cfd7e76dc2ad49a554026f10024a3696c2
vinibalbino <silvavinicius2018@gmail.com>
2022-07-26 18:59:55 -0400

    Adicionando porta para o cliente atraves da linha

--- client.py
+++ client.py
@@ -1,14 +1,24 @@
 import socket
+import sys
 
 #GET http://www.google.com/
 #GET http://www.github.com//
 
 HOST = 'localhost'
-PORT = 8080
+
+size_args = len(sys.argv)
+
+if size_args < 3:
+    print("Quantidades de argumentos invalidos!\n")
+    print("Exemplo: python3 -p <Porta> ")
+    sys.exit(-1)
+    
+PORT = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
 s.connect(('localhost',PORT))
 #se n funcionar,tentar url = str.encode() --> s.sendall(url)
+
 url = input("Request: ")
 s.send(url.encode())
 header = s.recv(1024) #vou receber algo de la-> HTTP 200 OK (HEADER)
